53c5ad5552a8a2154236e56f7950832578967821
Dominic Brant <dombrant@gmail.com>
2019-05-27 15:57:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Live Server
+
+## Synopsis
+
+A custom wrapper for @tapio's [live-server package](https://github.com/tapio/live-server).
+
+## Installation
+
+## Usage
+
+### From the command line:
+
+Call `live-server`
+
+### From Node:
+
+After cloning this repo, copy its contents into your project, then import it:
+
+```js
+const liveServer = require('lib/live-server/index.js);
+
+liveServer();
+```
+
+## Motivation
+
+I've really enjoyed using the `live-server` for development, but I wanted to create a simplified version that I can edit for myself. Specifically, this package displays both the local and remote (from the network's ip address) URLs (inspired by Zeit's [serve](https://github.com/zeit/serve)).
+
+## Contributors
+
+**Dominic Brant**.
+
+[Follow me on Twitter](https://twitter.com/dombrant). If you have any thoughts/suggestions/problems, feel free to open an issue or Tweet me.
+
+## License
+
+MIT© [Dominic Brant](https://dombrant.com)
---
+++ index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const liveServer = require("live-server");
+const ip = require("ip");
+const chalk = require("chalk");
+const clipboardy = require("clipboardy");
+
+const params = {
+  port: 3000,
+  root: "dist",
+  open: false,
+  logLevel: 0
+  // Set log level to 0 so it only logs errors
+};
+
+const launchServer = async () => {
+  const arguments = process.argv.slice(2);
+  [...arguments].toString();
+  // Make an array of any arguments the user passes
+  // This will allow the user to specify the port with the first argument
+  // and they can change the root folder from 'src' to 'dist'
+
+  if (arguments.length === 2) {
+    params.port = Number(arguments[0]);
+    params.root = arguments[1];
+    // If the user passes two arguments, make the port equal to the first argument
+    // and change the root folder to 'dist' if the user types it as the second argument
+  } else if (arguments.length === 1) {
+    params.port = Number(arguments[0]);
+    // If the user passes one argument, change the port to the argument passed
+  }
+
+  liveServer.start(params);
+
+  const localURL = `http://localhost:${params.port}`;
+  const remoteURL = `http://${ip.address()}:${params.port}`;
+
+  console.log(`\n${chalk.bold("Local:")}  ${localURL}`);
+  console.log(`${chalk.bold("Remote:")} ${remoteURL}`);
+
+  try {
+    await clipboardy.write(localURL);
+    console.log("\nLocal URL copied to clipboard!\n");
+  } catch (error) {
+    console.log(`${chalk.red("Error")} copying URL to clipboard: ${error}`);
+  }
+  // Use the clipboardy module to copy the localhost URL to the clipboard
+};
+
+launchServer();
+
+module.exports = launchServer;
